aff0dbbcf5500d24adbfbe687fdec14580661914
IanSaucy <IanSaucy@users.noreply.github.com>
2020-06-30 02:41:41 +0000

    deploy: CRANE-toolbox/covid-racism-tracker@679e843c602d54985ae06f749b62713400fd62bc

---
+++ precache-manifest.a49034f471d4fa95ffdc18eccada62c0.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da4487956c99bc5970581ad74b66f064",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ebd52efcfe295767a2c",
+    "url": "/static/css/2.b296d0e1.chunk.css"
+  },
+  {
+    "revision": "64ae3fc78cbe237c6581",
+    "url": "/static/css/main.cd4faea8.chunk.css"
+  },
+  {
+    "revision": "2ebd52efcfe295767a2c",
+    "url": "/static/js/2.310fbfb9.chunk.js"
+  },
+  {
+    "revision": "fdd30b2ab44ae896589b798d411d5c2c",
+    "url": "/static/js/2.310fbfb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ae3fc78cbe237c6581",
+    "url": "/static/js/main.b2a55004.chunk.js"
+  },
+  {
+    "revision": "da89f2d23661b0a094dd",
+    "url": "/static/js/runtime-main.6065443b.js"
+  },
+  {
+    "revision": "e93be79b55df78876970805357649cd2",
+    "url": "/static/media/LOGO.e93be79b.svg"
+  },
+  {
+    "revision": "c94f01c601eeffb0afad087fbddd182c",
+    "url": "/static/media/circle-close.c94f01c6.svg"
+  },
+  {
+    "revision": "244bb7e3221527f24b2a30635a24dd01",
+    "url": "/static/media/circle_email.244bb7e3.svg"
+  },
+  {
+    "revision": "f2ed3cd58fa084ff9c082f5e2dfd643a",
+    "url": "/static/media/devpost.f2ed3cd5.svg"
+  },
+  {
+    "revision": "7ae1bce6d6b0055de6e7daf22046ee4c",
+    "url": "/static/media/duel_person.7ae1bce6.svg"
+  },
+  {
+    "revision": "1b5f23372858924cda5f843b75f539bb",
+    "url": "/static/media/emma_profile.1b5f2337.jpg"
+  },
+  {
+    "revision": "07892031958b3a93df7565dc7ec7f06c",
+    "url": "/static/media/firstaid_kit.07892031.svg"
+  },
+  {
+    "revision": "3a5ed0225184d5d994566d4f31be4e9b",
+    "url": "/static/media/fist.3a5ed022.svg"
+  },
+  {
+    "revision": "2f13e468465cd1df3b1e08c7eea95b3a",
+    "url": "/static/media/flow-graph.2f13e468.svg"
+  },
+  {
+    "revision": "6bc1110cae7a686f78119b32c77c06e2",
+    "url": "/static/media/gianluca_profile.6bc1110c.jpg"
+  },
+  {
+    "revision": "07de12bbad8d8a45f59f0452c9282898",
+    "url": "/static/media/github.07de12bb.svg"
+  },
+  {
+    "revision": "2e1a2275270bb00956115d8bd4d15eee",
+    "url": "/static/media/graph-line.2e1a2275.svg"
+  },
+  {
+    "revision": "1a7c51eb2a338344b2b40fcc26b67aec",
+    "url": "/static/media/hands_together.1a7c51eb.svg"
+  },
+  {
+    "revision": "96a8a1cb4c369adbc31bff6c8be78219",
+    "url": "/static/media/heart_mob.96a8a1cb.svg"
+  },
+  {
+    "revision": "9d51dcc865ed069137afa160d72c240d",
+    "url": "/static/media/kelly_profile.9d51dcc8.jpg"
+  },
+  {
+    "revision": "bd0270f3a9da0c2596ef7e617a50d610",
+    "url": "/static/media/lina_profile.bd0270f3.jpg"
+  },
+  {
+    "revision": "0c2341db438aab592d330d9b2a34e171",
+    "url": "/static/media/rachael_profile.0c2341db.jpg"
+  },
+  {
+    "revision": "297977a356fc51c8f009321875ee726a",
+    "url": "/static/media/safe_horizon.297977a3.svg"
+  },
+  {
+    "revision": "6d1602ed9e8086feb4864e24881b4c52",
+    "url": "/static/media/single_person.6d1602ed.svg"
+  },
+  {
+    "revision": "e41fc2e3404929e58b859fc905494dc2",
+    "url": "/static/media/social_distancing.e41fc2e3.svg"
+  },
+  {
+    "revision": "ca76b603a465b6962c6f4f5525d72347",
+    "url": "/static/media/stats_chart.ca76b603.svg"
+  },
+  {
+    "revision": "3f987085f9d48edd95dc2c665584af34",
+    "url": "/static/media/svetlana_profile.3f987085.jpg"
+  },
+  {
+    "revision": "41b942af117e873adc271782ad89d03a",
+    "url": "/static/media/toolbox.41b942af.svg"
+  }
+]);
